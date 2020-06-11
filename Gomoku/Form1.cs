7739509ce1e9283650cbf7e9101b0b8bd871d299
Yuan <mlovyuan@yahoo.com.tw>
2020-06-11 16:24:11 +0800

    可隨意落子

---
+++ Gomoku/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    public partial class Form1 : Form
+    {
+        private bool Black;
+        public Form1()
+        {
+            InitializeComponent();
+            Black = true;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (Black)
+            {
+                this.Controls.Add(new BlackPiece(e.X, e.Y));
+                Black = false;
+            }
+            else
+            {
+                this.Controls.Add(new WhitePiece(e.X, e.Y));
+                Black = true;
+            }
+        }
+    }
+}
---
+++ Gomoku/Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gomoku
+{
+    // 棋子圖片來自PictureBox
+    abstract class Piece : PictureBox
+    {
+        private readonly static int ImageWidth = 50;
+        // 棋子放置的位置
+        public Piece(int x, int y)  
+        {
+            // 有this.BackColor欄位是因為繼承自PictureBox的關係
+            this.BackColor = Color.Transparent;
+            // 棋子放置的座標
+            this.Location = new Point(x - ImageWidth/2, y - ImageWidth / 2);
+            // 棋子的大小
+            this.Size = new Size(ImageWidth, ImageWidth);
+
+        }
+    }
+}
